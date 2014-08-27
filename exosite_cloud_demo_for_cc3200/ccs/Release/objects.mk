e80b64e017a9fca4074f39edce4838f5139b2e69
Patrick Barrett <patrickbarrett@exosote.com>
2014-08-27 15:06:01 -0500

    Clean Up

--- exosite_cloud_demo_for_cc3200/ccs/Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"libc.a" -l"C:/Users/Patrick/Desktop/CC3200CloudDemo-master/exosite_cloud_demo_for_cc3200/ccs/../../oslib/ccs/ti_rtos/ti_rtos.a" -l"C:/Users/Patrick/Desktop/CC3200CloudDemo-master/exosite_cloud_demo_for_cc3200/ccs/../../driverlib/ccs/Release/driverlib.a" -l"C:/Users/Patrick/Desktop/CC3200CloudDemo-master/exosite_cloud_demo_for_cc3200/ccs/../../simplelink/ccs/OS/simplelink.a"
-
